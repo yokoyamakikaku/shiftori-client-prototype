fdf7fdc85d45f3c17ba2e1b9f9cf92a85b1b6098
YOKOYAMA Taku <seekseep@gmail.com>
2025-01-05 15:34:14 +0900

    [ADD] react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const basename = import.meta.env.VITE_BASE_URL
+
+function Navigation () {
+  return (
+    <div>
+      <Link to="/">Hom</Link>
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+function Home () {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Navigation />
+    </div>
+  )
+}
+
+function About () {
+  return (
+    <div>
+      <h1>About</h1>
+      <Navigation />
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
-      <h1 className="p-12 font-bold text-4xl">Shiftori</h1>
+      <BrowserRouter basename={basename}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/shiftori-client-prototype/',
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/', // 環境変数を使用
+  };
+});
