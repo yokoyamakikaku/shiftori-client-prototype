eef5e313eebacc18e2f17d7df260798928f4d904
YOKOYAMA Taku <seekseep@gmail.com>
2025-01-05 15:49:05 +0900

    [UPDATE] vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   console.log('mode:', mode);
   const env = loadEnv(mode, process.cwd(), '');
-
+  const base = env.BASE_URL || '/';
   return {
     plugins: [react()],
-    base: env.BASE_URL || '/', // 環境変数を使用
+    base
   };
 });
