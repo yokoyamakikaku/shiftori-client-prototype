e06786cdf34b056ca9c1d4f0176ecb835315177e
YOKOYAMA Taku <seekseep@gmail.com>
2025-01-05 15:55:30 +0900

    [DELETE] console.log

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  console.log('mode:', mode);
   const env = loadEnv(mode, process.cwd(), '');
   const base = env.BASE_URL || '/';
   return {
